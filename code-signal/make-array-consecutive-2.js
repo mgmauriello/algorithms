a8c481dc2abaf16d3db083617817dc7c22d70cb3
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-03-20 22:05:38 +0000

    completed more algos.

---
+++ code-signal/make-array-consecutive-2.js
@@ -0,0 +1,15 @@
+function consecutiveArr(statues) {
+  var statuesNeeded = 0;
+  // Sort array smallest to largest
+  statues.sort((a, b) => {
+    return a - b;
+  })
+  // Iterate through array and find gaps in values
+  for (let i = 0; i < statues.length; i++) {
+    // If there is a gap between neighboring numbers subtract higher number from lower number i.e. [3, 6] is 6 - 3. There is a gap of 3, so 2 numbers are missing i.e. 4, 5
+    if (statues[i + 1] - statues[i] > 1) {
+      statuesNeeded += statues[i + 1] - statues[i] - 1;
+    }
+  }
+  return statuesNeeded;
+}
