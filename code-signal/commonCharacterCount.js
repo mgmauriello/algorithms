e52abba36fa194607ee3a96bb7fcf42a345068e1
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-04-24 20:54:07 +0000

    common character count complete

---
+++ code-signal/commonCharacterCount.js
@@ -0,0 +1,14 @@
+function commonCharacterCount(s1, s2) {
+    let count = 0;
+    s1 = s1.split('');
+    s2 = s2.split('');
+
+    s1.forEach(e => {
+      if (s2.includes(e)) {
+        count++;
+        s2.splice(s2.indexOf(e), 1);
+      }
+    });
+
+    return count;
+}
